32b3de2276a7f8071f55f1ed08955b6c5e2b23b6
Mariusz Klimek <klimcio@gmail.com>
2026-06-15 21:51:15 +0200

    Credits scroll from bottom to top

--- pong/src/credits_plugin.rs
+++ pong/src/credits_plugin.rs
@@ -4,58 +4,55 @@ use crate::components::{Config, GameState};
 #[derive(Component)]
 pub struct CreditsText;
 
-#[derive(Resource)]
-pub struct CreditsTimer(pub Timer);
-
 pub struct CreditsPlugin;
 
 impl Plugin for CreditsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::Credits), show_credits)
             .add_systems(OnExit(GameState::Credits), hide_credits)
-            .add_systems(Update, credits_timeout.run_if(in_state(GameState::Credits)));
+            .add_systems(Update, scroll_credits.run_if(in_state(GameState::Credits)));
     }
 }
 
+const SCROLL_SPEED: f32 = 200.0;
+
 fn show_credits(mut commands: Commands, config: Res<Config>) {
+    let start_top = config.screen.height as f32 + 50.0;
+
     commands.spawn((
+        Text::new("LOL"),
+        TextFont { font_size: config.game.font_size, ..default() },
+        TextColor(Color::WHITE),
+        TextLayout::new(Justify::Center, LineBreak::NoWrap),
         Node {
             position_type: PositionType::Absolute,
             left: Val::Px(0.0),
             right: Val::Px(0.0),
-            top: Val::Px(0.0),
-            bottom: Val::Px(0.0),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
+            top: Val::Px(start_top),
             ..default()
         },
         CreditsText,
-    )).with_children(|parent| {
-        parent.spawn((
-            Text::new("LOL"),
-            TextFont { font_size: config.game.font_size, ..default() },
-            TextColor(Color::WHITE),
-            TextLayout::new(Justify::Center, LineBreak::NoWrap),
-        ));
-    });
-
-    commands.insert_resource(CreditsTimer(Timer::from_seconds(5.0, TimerMode::Once)));
+    ));
 }
 
 fn hide_credits(mut commands: Commands, query: Query<Entity, With<CreditsText>>) {
     for entity in query.iter() {
         commands.entity(entity).despawn();
     }
-    commands.remove_resource::<CreditsTimer>();
 }
 
-fn credits_timeout(
+fn scroll_credits(
     time: Res<Time>,
-    mut timer: ResMut<CreditsTimer>,
     mut next_state: ResMut<NextState<GameState>>,
+    mut query: Query<&mut Node, With<CreditsText>>,
 ) {
-    timer.0.tick(time.delta());
-    if timer.0.just_finished() {
-        next_state.set(GameState::Menu);
+    for mut node in query.iter_mut() {
+        if let Val::Px(top) = &mut node.top {
+            *top -= SCROLL_SPEED * time.delta_secs();
+
+            if *top < -50.0 {
+                next_state.set(GameState::Menu);
+            }
+        }
     }
 }
